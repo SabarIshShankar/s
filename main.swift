d366b253a5b81e7cac802bd9cdd49d7abe34e734
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-10-02 17:10:36 +0530

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,52 @@
+//1
+print("Hello world")
+print(5+6)
+var str=5+7
+print(str)//12
+
+var apples = 5
+var oranges = 5
+var fruit = apples + oranges
+print(fruit)//10
+
+print("Items to print", separator: "Value " , terminator: "Valuet")//Items to printValuet
+
+print("Value one")
+//Value one
+
+print("Value one","Value two", separator: " Next Value" , terminator: " End")
+//Value one Next Value 
+//Value two End
+
+
+typealias Feet = Int
+var distance: Feet = 100
+print("\n", distance )
+//100
+
+
+var pi = 3.14159
+print(pi*2)
+
+//type anotations
+
+var varA = 42;
+print(varA)
+
+var varB:Float
+varB = 3.14
+print(varB)
+
+var varC:Double
+varC = 33.11
+print(varC)
+
+var str1 = "Hello"
+print(str1)
+
+var varD:String
+varD = "what guy"
+print(varD)
+
+
+print("\n Value of \(varD) is string and \(varC) is double")
